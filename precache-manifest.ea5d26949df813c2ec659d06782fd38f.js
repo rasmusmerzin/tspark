1fe0713045e764b746694f018e936a6fc86609c7
rasmusmerzin <rasmusmerzin@gmail.com>
2020-02-24 02:23:54 +0200

    Updates

---
+++ precache-manifest.ea5d26949df813c2ec659d06782fd38f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f8ddb049bc1ebfc80c134b2ca729b59",
+    "url": "/tspark/index.html"
+  },
+  {
+    "revision": "3317a3c2a687e68d8359",
+    "url": "/tspark/static/css/main.a5e5e3d2.chunk.css"
+  },
+  {
+    "revision": "8e97c9609cac1bcc64eb",
+    "url": "/tspark/static/js/2.17ce77e9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/tspark/static/js/2.17ce77e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3317a3c2a687e68d8359",
+    "url": "/tspark/static/js/main.f65c7fb9.chunk.js"
+  },
+  {
+    "revision": "e7b7500000a56ae3da42",
+    "url": "/tspark/static/js/runtime-main.6b0a46fd.js"
+  }
+]);
